b8040fec25500fd05e8e250b2dd385279a5cdb94
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-09-23 13:11:46 +0800

    1.npm install -g @angular/cli@latest 2.ng new my-app-shell --routing 3.ng build --prod 4.ng serve --prod 5.ng generate app-shell 6.ng run my-app-shell:app-shell:production 7.npm install http-server 8.package.json add     "build:prod:shell": "ng run my-app-shell:app-shell:production",     "prod": "npm run build:prod:shell && cd dist && http-server -p 4200 -c-1" 9.ng run prod 10.http://localhost:4200/my-app-shell/browser/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
